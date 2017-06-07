74be0c83d2c14df3398c11c68e19b1aed26ab012
Guðjón Geir <gudjongeir@gmail.com>
2017-06-07 23:09:38 +0000

    Added a listview with test data

--- TempoLogger/MainWindow.xaml.cs
+++ TempoLogger/MainWindow.xaml.cs
@@ -12,17 +12,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TempoLogger.Models;
 
 namespace TempoLogger
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var logList = new List<WorkLog>
+			{
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+				new WorkLog
+				{
+					Issue = "AB-123",
+					Title = "Test issue",
+					From = "13:00",
+					To = "14:00",
+					Duration = "1h"
+				},
+			};
+
+			logs.ItemsSource = logList;
+		}
+
+	}
 }
---
+++ TempoLogger/Models/WorkLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TempoLogger.Models
+{
+	class WorkLog
+	{
+		public string Issue { get; set; }
+		public string Title { get; set; }
+		public string From { get; set; }
+		public string To { get; set; }
+		public string Duration { get; set; }
+		public DateTime Date { get; set; }
+		public string Account { get; set; }
+		public string Comment { get; set; }
+	}
+}
