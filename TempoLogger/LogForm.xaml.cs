560db84786825397dee27c8938a07cb4373a8113
Guðjón Geir <gudjongeir@gmail.com>
2017-06-10 23:10:08 +0000

    When start and end lose focus their content is reformatted

--- TempoLogger/LogForm.xaml.cs
+++ TempoLogger/LogForm.xaml.cs
@@ -64,7 +64,7 @@ namespace TempoLogger
 			var timeRegex = @"^[0-9][0-9]?:[0-9]{2}$";
 			//var durationRegex = @"^(([0-9]+)h)?\s*(([0-9]+)m)?$";
 
-			if (!Regex.Match(TxtIssue.Text, issueRegex).Success)
+			if (!Regex.IsMatch(TxtIssue.Text, issueRegex))
 			{
 				LblError.Content = "Issue is not in correct format";
 				return false;
@@ -76,19 +76,19 @@ namespace TempoLogger
 				return false;
 			}
 
-			if (!Regex.Match(TxtStart.Text, timeRegex).Success)
+			if (!Regex.IsMatch(TxtStart.Text, timeRegex))
 			{
 				LblError.Content = "Start is not in correct format";
 				return false;
 			}
 
-			if (!Regex.Match(TxtEnd.Text, timeRegex).Success)
+			if (!Regex.IsMatch(TxtEnd.Text, timeRegex))
 			{
 				LblError.Content = "End is not in correct format";
 				return false;
 			}
 
-			//if (string.IsNullOrEmpty(TxtDuration.Text) ||!Regex.Match(TxtDuration.Text, durationRegex).Success)
+			//if (string.IsNullOrEmpty(TxtDuration.Text) ||!Regex.IsMatch(TxtDuration.Text, durationRegex))
 			//{
 			//	LblError.Content = "Duration is not in correct format";
 			//	return false;
@@ -102,6 +102,37 @@ namespace TempoLogger
 
 			LblError.Content = "";
 			return true;
+		}
+
+		private void TxtStart_LostFocus(object sender, RoutedEventArgs e)
+		{
+			TxtStart.Text = FormatTimeString(TxtStart.Text);
+		}
+
+		private void TxtEnd_LostFocus(object sender, RoutedEventArgs e)
+		{
+			TxtEnd.Text = FormatTimeString(TxtEnd.Text);
+		}
+
+		private string FormatTimeString(string timeString)
+		{
+			var regex = @"^([0-9][0-9]?):?([0-9]{2})?$";
+
+			var match = Regex.Match(timeString, regex);
+
+			if (!match.Success) return timeString;
+
+			var hourStr = match.Groups[1].Value;
+
+			var minuteStr = match.Groups.Count == 3
+				? match.Groups[2].Value
+				: "00";
+
+			if (int.Parse(hourStr) > 23 || int.Parse(minuteStr) > 59) return timeString;
+
+			hourStr = hourStr.Length == 1 ? "0" + hourStr : hourStr;
+
+			return hourStr + ":" + minuteStr;
 		}
 	}
 }
